5e213ffd592bbbf79ea7d5b7cc1f8bd216c1b04b
Ali HamXa <alihamzatirmazi@gmail.com>
2021-10-08 18:50:35 +0500

    update app

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-var timer = null
-var currentDate = document.querySelector("h1");
-var clockDisplay = document.querySelector("h2");
+let timer = null;
+let currentDate = document.querySelector("h1");
+let clockDisplay = document.querySelector("h2");
+let quote = document.querySelector("#quote");
+let focus = document.querySelector("#focus");
 
-function start(){
-    var now = new Date();
-    var date = now.toDateString();
-    var clock = now.toLocaleTimeString();
+if (localStorage.getItem('focus')) {
+    focus.value = localStorage.getItem('focus');
+}
 
-    currentDate.innerHTML = clock; 
+const todayFocus = () => {
+    const value = focus.value;
+    localStorage.setItem('focus', value);
+    focus.disabled = true;
+}
+
+const getQuote = async () => {
+    const res = await fetch('https://private-anon-afa0d85240-goquotes.apiary-proxy.com/api/v1/random?count=1');
+    res.json().then(data => {
+        quote.innerHTML = data.quotes[0].text;
+    }
+    )
+};
+
+function start() {
+    let now = new Date();
+    let date = now.toDateString();
+    let clock = now.toLocaleTimeString();
+
+    currentDate.innerHTML = clock;
     clockDisplay.innerHTML = date;
-    timer = setTimeout("start()",1000);
+    timer = setTimeout("start()", 1000);
 }
 
 start();
+getQuote();
